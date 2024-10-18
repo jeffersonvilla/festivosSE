c5a9c537394f2571563f3accdb4ae9828fcd28dc
wendyres <wen.rstrpo@gmail.com>
2024-10-18 01:03:35 -0500

    agregado metodo obtener

--- repositorio/festivo.repositorio.js
+++ repositorio/festivo.repositorio.js
@@ -26,4 +26,27 @@ FestivoRepositorio.listarFestivos = async (respuesta) => {
     }
 }
 
+FestivoRepositorio.obtenerFestivosPorAnio = async (anio, respuesta) => {
+    const basedatos = bd.obtenerBD();
+    try {
+        const resultado = await basedatos.collection('tipos')
+            .aggregate([
+                { $unwind: "$festivos" },
+                { $match: { "festivos.anio": anio } }, // Filtrar por el año
+                { $project: {
+                    _id: 0, // Excluir el _id de la salida
+                    dia: '$festivos.dia', 
+                    mes: '$festivos.mes', 
+                    nombre: '$festivos.nombre'
+                }}
+            ]).toArray();
+        
+        return respuesta(null, resultado);
+    } catch (error) {
+        console.error('Error al obtener los festivos:', error);
+        respuesta(error, null);
+    }
+}
+
+
 module.exports = FestivoRepositorio;
