b40524278a92d4010ec4888e727c09b35cbf458b
wendyres <wen.rstrpo@gmail.com>
2024-10-18 14:29:59 -0500

    repositorio actualizado

--- repositorio/festivo.repositorio.js
+++ repositorio/festivo.repositorio.js
@@ -1,30 +1,3 @@
-const bd = require('./bd');
-
-const FestivoRepositorio = () => { };
-
-FestivoRepositorio.listarFestivos = async (respuesta) => {
-    const basedatos = bd.obtenerBD();
-    try {
-        const resultado = await
-            basedatos.collection('tipos')
-                .aggregate([
-                    { $unwind: "$festivos" },
-                    { $project: {
-                        id: '$id',
-                        dia: '$festivos.dia', 
-                        mes: '$festivos.mes', 
-                        nombre: '$festivos.nombre', 
-                        diasPascua: '$festivos.diasPascua'
-                    }}
-                ]).toArray();
-        
-        return respuesta(null, resultado);
-    }
-    catch (error) {
-        console.error('Error al listar los festivos:', error);
-        respuesta(error, null);
-    }
-}
 
 const bd = require('./bd');
 
